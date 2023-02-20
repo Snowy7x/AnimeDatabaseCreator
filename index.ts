53110b463d3c64a68c6633e8b2035c65f71348f4
Snowy7 <snowydev7@gmail.com>
2023-02-20 23:55:14 +0200

    first commit

--- index.ts
+++ index.ts
@@ -13,6 +13,7 @@ server.listen(3000, () => {
 import axios, { AxiosError } from "axios";
 import {
   getAnime,
+  getAnimeList,
   getEpisodesList,
   getWatchLinks,
 } from "./src/sources/animeslayer.js";
@@ -25,8 +26,11 @@ import {
   getAnimeByName,
   getAnimeByNameWithEpisodes,
   getEpisodesWithId,
+  malGetAnimeWithId,
+  malGetMangaWithId,
 } from "./src/sources/myanimelist.js";
 import { kill } from "process";
+import { fetchZoroAnimeFromName } from "./src/sources/zoro.js";
 
 const AutoIncrement = Inc(mongoose);
 
@@ -37,7 +41,7 @@ const T_Schema = new Schema({
 
 const EpisodeDetails = new Schema({
   id: { type: Number, default: null },
-  enId: { type: Number, default: null },
+  enId: { type: String, default: null },
   name: { type: String, default: null },
   number: { type: String, default: null },
   thumbnailUrl: { type: String, default: null },
@@ -63,12 +67,22 @@ const Recommendation = new Schema({
   coverUrl: { type: String, default: null },
 });
 
+const Adaption = new Schema({
+  id: { type: Number, default: null },
+  mal_id: { type: Number, default: null },
+  ani_id: { type: Number, default: null },
+  name: { type: String, default: null },
+  coverUrl: { type: String, default: null },
+  type: { type: String, default: null },
+});
+
 const AnimeSchema = new Schema({
   id: { type: Number, default: null, sparse: true },
   mal_id: { type: Number, default: null },
   ani_id: { type: Number, default: null },
   as_id: { type: Number, default: null },
   justInfo: { type: Boolean, default: false },
+  adaption: { type: Adaption, default: null },
 
   name: { type: String, default: null },
   description_ar: { type: String, default: null },
@@ -97,210 +111,308 @@ const AnimeSchema = new Schema({
   recommended: [Recommendation],
 });
 
-AnimeSchema.plugin(AutoIncrement, { inc_field: "id" });
+const MangaSchema = new Schema({
+  id: { type: Number, default: null, sparse: true },
+  mal_id: { type: Number, default: null },
+  ani_id: { type: Number, default: null },
 
-const AnimeModal = model("Anime", AnimeSchema);
+  name: { type: String, default: null },
+  description_ar: { type: String, default: null },
+  description_en: { type: String, default: null },
+  coverUrl: { type: String, default: null },
+  bannerUrl: { type: String, default: null },
+  source: { type: String, default: null },
 
-const headers = {
-  "Client-Id": "android-app2",
-  "Client-Secret": "7befba6263cc14c90d2f1d6da2c5cf9b251bfbbd",
+  score: { type: Number, default: null },
+  scored_by: { type: Number, default: null },
+  year: { type: Number, default: null },
+
+  type: { type: String, default: null },
+  status: { type: String, default: null },
+  keywords: { type: [String], default: [] },
+
+  genres_ar: [T_Schema],
+  genres_en: [T_Schema],
+  studios: [T_Schema],
+  chapters: [EpisodeDetails],
+  relations: [Relation],
+  recommended: [Recommendation],
+});
+
+const LatestEpisodeSchema = new Schema({
+  id: { type: Number, default: null, sparse: true },
+  mal_id: { type: Number, default: null },
+  ani_id: { type: Number, default: null },
+  epId: { type: Number, default: null },
+  epIdEn: { type: String, default: null },
+
+  epNumber: { type: Number, default: null },
+  name: { type: String, default: null },
+  description_ar: { type: String, default: null },
+  description_en: { type: String, default: null },
+  coverUrl: { type: String, default: null },
+
+  score: { type: Number, default: null },
+  scored_by: { type: Number, default: null },
+
+  type: { type: String, default: null },
+  status: { type: String, default: null },
+
+  genres_ar: [T_Schema],
+  genres_en: [T_Schema],
+});
+
+const topAnimeSchema = new Schema({
+  id: { type: Number, default: null, sparse: true },
+  mal_id: { type: Number, default: null },
+  ani_id: { type: Number, default: null },
+
+  name: { type: String, default: null },
+  description_ar: { type: String, default: null },
+  description_en: { type: String, default: null },
+  coverUrl: { type: String, default: null },
+
+  score: { type: Number, default: null },
+  scored_by: { type: Number, default: null },
+
+  type: { type: String, default: null },
+  status: { type: String, default: null },
+
+  genres_ar: [T_Schema],
+  genres_en: [T_Schema],
+});
+
+type zoroEpisode = { epNum: string; episodeName: string; episodeId: string };
+type anEpisode = {
+  episode_number: string;
+  episode_name: string;
+  episode_id: string;
+  allow_comment: string;
+  skip_from: string;
+  skip_to: string;
+  episode_watched_history: any;
+  next_episode: anEpisode;
+  previous_episode: anEpisode;
 };
 
-async function createAnime(d: any) {
-  const anime = new AnimeModal({
-    as_id: d.anime_id,
+const AnimeModal = model("Anime", AnimeSchema);
+const MangaModal = model("Manga", MangaSchema);
+const LatestEpisodeModal = model("LatestEpisode", LatestEpisodeSchema);
+const topAnimeModal = model("topAnime", topAnimeSchema);
 
-    name: d.anime_name,
-    description_ar: d.anime_description,
-    coverUrl: d.anime_cover_image_full_url ?? d.anime_cover_image_url,
-    bannerUr: d.anime_banner_image_url,
-    source: d?.more_info_result?.source,
-
-    year: d.anime_release_year,
-
-    type: d.anime_type,
-    Rated: d.anime_age_rating,
-    season: d.anime_season,
-    status: d.anime_status,
-
-    genres_ar: d?.anime_genres?.split(", ").map((e, i) => ({
-      name: e,
-      id: d?.anime_genre_ids?.split(", ")[i],
-    })),
-  });
-  await anime.save();
-}
-
-//getEpisodesWithId(53446).then((ep) => console.log("Episodes: ", ep));
-getWatchLinks(2395, 42838).then((links) => console.log(links));
+updateLatestEpisodes();
 
 // TODO: 3849 requires update
 // TODO: animes with ani_id: 102416
-mongoose.connection.on("open", async () => {
-  let promises = [];
-  let docs = AnimeModal.find({
-    mal_id: { $ne: null },
-    status: { $ne: "Not Yet Aired" },
-    as_id: { $lte: 3009 },
-    "episodes.0": { $exists: false },
-  });
-  let count = await docs.clone().count();
-  console.log(count);
-  // TODO: Update the episodes
-  for await (const doc of (await docs).reverse()) {
-    let eps = await getEpisodesList(doc.as_id);
+mongoose.connection.on("open", async () => {});
 
-    if (eps.code === 400 || eps === null || eps.data.length === 0) {
-      console.log("No episodes found for", doc.id);
-      continue;
-    }
-
-    if (doc.episodes.length >= eps.data.length) continue;
-    console.log(`Fetching episodes watch links[${doc.id}]: ` + eps.data.length);
-    let videos = [];
-    if (doc.year < 2001 && doc.status == "Finished Airing") videos = [];
-    else videos = await getEpisodesWithId(doc.mal_id);
-    let episodes = [];
-    for await (const ep of eps.data) {
-      const ep2 = videos?.find((p) => p.episode === ep.episode_number);
-      let urls = await getWatchLinks(doc.as_id, ep.episode_id);
-      episodes.push({
-        id: ep.episode_id,
-        name: ep.episode_name,
-        number: ep.episode_number,
-        thumbnail: ep2?.images?.jpg?.maximum_image_url
-          ? ep2?.images?.jpg?.maximum_image_url
-          : ep2?.images?.webp?.maximum_image_url
-          ? ep2?.images?.webp?.maximum_image_url
-          : doc.coverUrl,
-        urls: urls,
-      });
-    }
-    doc.episodes = new Types.DocumentArray(episodes);
-    await doc.save();
-    console.log("Updated episodes: ", episodes.length);
-  }
-  // TODO: Updating the rest...
-  /* for await (const doc of docs) {
-    console.log("Getting anime[anime slayer]: " + doc.as_id);
-    let anime = await getAnime(doc.as_id);
-    if (!anime) {
-      console.log("Could not find anime[anime slayer]: " + doc.as_id);
-      continue;
-    } else {
-      console.log("got anime[anime slayer]: " + anime.anime_keywords);
-    }
-    let mal_data = null;
-    let keywords = anime.anime_keywords;
-    if (
-      !keywords ||
-      keywords.length <= 2 ||
-      keywords === "," ||
-      keywords === " ," ||
-      keywords === ", "
-    ) {
-      mal_data = await getAnimeByName(doc.name);
-    } else {
-      for await (const keyword of keywords.split(",")) {
-        if (
-          !keyword ||
-          keyword.length <= 1 ||
-          keyword === "," ||
-          keyword === " ," ||
-          keyword === ", "
-        )
-          continue;
-        const mal_data = await getAnimeByName(keyword);
-        if (!mal_data || mal_data.mal_id == null) continue;
-        else break;
-      }
-    }
-    if (mal_data != null) {
-      if (!mal_data || mal_data.mal_id == null) continue;
-      console.log("Updating anime: " + mal_data.title);
-      doc.description_en = mal_data.synopsis;
-      doc.mal_id = mal_data.mal_id;
-      doc.ani_id = null;
-      doc.duration = mal_data?.duration?.toString();
-      doc.source = mal_data.source;
-      doc.score = mal_data.score;
-      doc.scored_by = mal_data.scored_by;
-      doc.trailer = mal_data?.trailer.url;
-      doc.genres_en = new Types.DocumentArray(
-        mal_data.genres?.map((re, ind) => ({
-          id: re.mal_id,
-          name: re.name,
-        }))
-      );
-      doc.coverUrl = mal_data.images.jpg?.maximum_image_url
-        ? mal_data.images.jpg?.maximum_image_url
-        : mal_data.images.webp?.maximum_image_url;
-      doc.bannerUr = mal_data.images.jpg?.maximum_image_url;
-      doc.studios = new Types.DocumentArray(
-        mal_data.studios?.map((e) => ({
-          name: e.name,
-          id: e.mal_id,
-        }))
-      );
-
-      doc.keywords = keywords
-        .split(",")
-        .filter((keyword) => keyword.length > 0)
-        .concat(
-          mal_data?.title_synonyms?.filter(
-            (item) => keywords.split(",").indexOf(item) < 0
-          )
-        );
-      await doc.save();
-    }
-    //await UpdateAnime(doc);
-  } */
-});
-
-/* async function UpdateAnime(doc) {
-  let mal_data =
-    doc.ani_id >= 1
-      ? await getAnimeById(doc.ani_id)
-      : await getAnimeByName(doc.name);
-  if (!mal_data || mal_data.id == null) {
-    console.log("Anime not found[Updating using animeslayer]: " + doc.name);
-    let anime = await getAnime(doc.as_id);
-    doc.source = anime?.more_info_result?.source;
-    doc.trailer = anime?.more_info_result?.trailer_url;
-    doc.score = anime?.more_info_result?.score;
-    doc.score_by = anime?.more_info_result?.score_by;
-    doc.duration = anime?.more_info_result?.duration;
-    doc.coverUrl = anime.anime_cover_image_full_url
-      ? anime.anime_cover_image_full_url
-      : anime.anime_cover_image_url;
-    doc.bannerUr = anime?.anime_banner_image_url
-      ? anime.anime_banner_image_url
-      : doc.coverUrl;
-    doc.studios = anime?.more_info_result?.studios;
-  } else {
-    doc.description_en = mal_data.description;
-    doc.mal_id = mal_data.idMal;
-    doc.ani_id = mal_data.id;
-    doc.duration = mal_data.duration.toString();
-    doc.source = mal_data.source;
-    doc.score = (mal_data.averageScore / 100) * 10;
-    doc.trailer = mal_data.trailer;
-    doc.genres_en = new Types.DocumentArray(
-      mal_data.genres?.map((re, ind) => ({
-        id: ind,
-        name: re,
-      }))
+async function updateLatestEpisodes() {
+  //  TODO: Update the latest episodes
+  const latestEpisodes = await getAnimeList("latest_episodes", 1, 0).then(
+    (r) => r.data
+  );
+  for await (let episode of latestEpisodes) {
+    const anime = await AnimeModal.findOne({ as_id: episode.anime_id });
+    let zoroAnime = await fetchZoroAnimeFromName(anime.name).then(
+      (an) => an?.episodes[an?.episodes?.length - 1]
     );
-    doc.coverUrl = mal_data.coverImage.large;
-    doc.bannerUr = mal_data.bannerImage;
-    doc.studios = new Types.DocumentArray(
-      mal_data.studios?.map((e) => ({
-        name: e.name,
-        id: e.id,
-      }))
-    );
+    const latestEpisode = new LatestEpisodeModal({
+      id: anime.id,
+      mal_id: anime.mal_id,
+      ani_id: anime.ani_id,
+      epId: episode.latest_episode_id,
+      epIdEn: zoroAnime[zoroAnime?.length - 1]?.episodeId,
+
+      epNumber: episode.latest_episode_name.replace("الحلقة : ", ""),
+      name: anime.name,
+      description_ar: anime.description_ar,
+      description_en: anime.description_en,
+      coverUrl: anime.coverUrl,
+
+      score: anime.score,
+      scored_by: anime.scored_by,
+
+      type: anime.type,
+      status: anime.status,
+
+      genres_ar: anime.genres_ar,
+      genres_en: anime.genres_en,
+    });
+    await latestEpisode.save();
+    await UpdateFull(anime);
   }
-  await doc.save();
 }
- */
-async function updateWithKeywords(doc) {}
+
+async function updateTopAnime() {
+  // TODO: Update the top animes
+  const topAnimes = await getAnimeList("top_currently_airing_mal", 1, 0).then(
+    (r) => r.data
+  );
+  let i = 1;
+  for await (let an of topAnimes) {
+    const anime = await AnimeModal.findOne({ as_id: an.anime_id });
+
+    const topAnime = new topAnimeModal({
+      id: anime.id,
+      mal_id: anime.mal_id,
+      ani_id: anime.ani_id,
+
+      name: anime.name,
+      description_ar: anime.description_ar,
+      description_en: anime.description_en,
+      coverUrl: anime.coverUrl,
+
+      rank: i,
+      score: anime.score,
+      scored_by: anime.scored_by,
+
+      type: anime.type,
+      status: anime.status,
+
+      genres_ar: anime.genres_ar,
+      genres_en: anime.genres_en,
+    });
+    await topAnime.save();
+    i++;
+  }
+}
+
+async function UpdateFull(doc) {
+  console.log("Updating:", doc.id);
+  await getAnime(doc.as_id)
+    .then(async (anime) => {
+      if (!anime) return console.log("Got issues with the anime");
+      const ji = anime.just_info == "Yes" || anime.just_info == "YES";
+      doc.justInfo = ji;
+      if (!ji) {
+        let anime = await malGetAnimeWithId(doc.mal_id);
+        console.log("title:", anime.title);
+        doc.name = anime.title;
+
+        // Update Relations
+        if (anime.relations && anime.relations.length > 0) {
+          console.log("Updating relations:", anime.relations.length);
+          let final_relations = [];
+
+          for await (const relation of anime.relations) {
+            if (
+              relation.relation == "Adaptation" ||
+              relation.relation == "adaptation"
+            ) {
+              const source = await malGetMangaWithId(relation.entry[0].mal_id);
+              console.log("Adaptation:", source.title);
+              // Updating manga info:
+              let manga = await MangaModal.findOne({ mal_id: source.mal_id });
+              if (!manga) {
+                manga = new MangaModal({
+                  mal_id: source.mal_id,
+                  name: source.title,
+                  description_ar: "",
+                  description_en: source.synopsis,
+                  coverUrl: source.images.jpg.image_url,
+                  bannerUrl: source.images.jpg.image_url,
+                  source: null,
+                  score: source.score,
+                  scored_by: source.scored_by,
+                  year: source.published.from.split("-").shift(),
+                  type: source.type,
+                  status: source.status,
+                  keywords: source.titles.map((title) => title.title),
+                  genres_en: source.genres.map((genre) => ({
+                    id: genre.mal_id,
+                    name: genre.name,
+                  })),
+                  studios: source.serializations.map((ser) => ({
+                    id: ser.mal_id,
+                    name: ser.name,
+                  })),
+                });
+                await manga.save();
+              }
+              // Add the manga
+              doc.adaptation = {
+                id: manga.id,
+                mal_id: source?.mal_id,
+                ani_id: -1,
+                name: source?.title,
+                coverUrl: source?.images.jpg?.maximum_image_url
+                  ? source?.images.jpg.maximum_image_url
+                  : source?.images.webp?.maximum_image_url
+                  ? source?.images.webp.maximum_image_url
+                  : null,
+                type: source.type,
+              };
+            } else {
+              for (const entry of relation.entry) {
+                const rel =
+                  (await AnimeModal.findOne({ mal_id: entry.mal_id })) ?? null;
+
+                final_relations.push({
+                  id: rel?.id,
+                  mal_id: rel?.mal_id,
+                  ani_id: rel?.ani_id,
+                  as_id: rel?.as_id,
+                  coverUrl: rel?.coverUrl,
+                  rating: rel?.score,
+                  type: rel?.type,
+                });
+              }
+            }
+          }
+          doc.relations = final_relations;
+        } else {
+          console.log("No Relations found for this anime");
+        }
+
+        // Update Episodes:
+        const promises = [
+          new Promise<anEpisode[]>((resolve) => {
+            getEpisodesList(doc.as_id).then((episodes) =>
+              resolve(episodes?.data)
+            );
+          }),
+          new Promise<zoroEpisode[]>((resolve) => {
+            fetchZoroAnimeFromName(anime.title).then((an) =>
+              resolve(an?.episodes)
+            );
+          }),
+        ];
+
+        await Promise.all(promises)
+          .then(async (re) => {
+            const ar = <anEpisode[]>re[0];
+            const en = <zoroEpisode[]>re[1];
+            console.log(`Ar episodes for [${doc.id}] ` + ar.length);
+            console.log(`En episodes for [${doc.id}] ` + en.length);
+            let count = ar.length < en.length ? ar.length : en.length;
+            let episodesFinal = [];
+            for (let i = 0; i < count; i++) {
+              const arEp = ar[i];
+              const enEp = en[i];
+              let ep = {
+                id: arEp?.episode_id,
+                enId: enEp?.episodeId,
+                name: enEp?.episodeName
+                  ? enEp?.episodeName
+                  : arEp?.episode_name,
+                number: enEp?.epNum ? enEp?.epNum : arEp?.episode_number,
+                thumbnailUrl: doc?.episodes[i]?.thumbnailUrl,
+                urls: doc?.episodes[i]?.urls,
+              };
+              episodesFinal.push(ep);
+            }
+            doc.episodes = new Types.DocumentArray(episodesFinal);
+            console.log("Updated episodes");
+            await doc.save();
+          })
+          .catch((err) => {
+            console.log(
+              `Error saving episodes for [${doc.id}] because:`,
+              err.message
+            );
+          });
+      }
+      await doc.save();
+    })
+    .catch((err) => console.log(err.message));
+}
